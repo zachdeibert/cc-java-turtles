707b9f6c78adf41a367e65bab28a8c9070868f1d
Zach Deibert <zachdeibert@gmail.com>
2016-08-14 16:52:58 -0500

    Added start guide

--- src/main/java/com/github/zachdeibert/ccjavaturtles/WebServer.java
+++ src/main/java/com/github/zachdeibert/ccjavaturtles/WebServer.java
@@ -1,10 +1,27 @@
 package com.github.zachdeibert.ccjavaturtles;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+import org.apache.commons.io.IOUtils;
+
 import fi.iki.elonen.NanoHTTPD;
+import fi.iki.elonen.NanoHTTPD.Response.Status;
 
 class WebServer extends NanoHTTPD {
 	@Override
 	public Response serve(IHTTPSession session) {
+		System.out.println(session.getUri());
+		if ( session.getUri().equals("/") ) {
+			try ( InputStream in = WebServer.class.getResourceAsStream("help.html") ) {
+				return newFixedLengthResponse(Status.OK, "text/html", IOUtils.toString(in, Charset.defaultCharset())
+						.replace("{{ port }}", String.valueOf(getListeningPort())));
+			} catch ( IOException ex ) {
+				ex.printStackTrace();
+				return newFixedLengthResponse(Status.INTERNAL_ERROR, "text/plain", ex.toString());
+			}
+		}
 		return newFixedLengthResponse("Hello, world!");
 	}
 
